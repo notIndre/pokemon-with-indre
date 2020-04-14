a61326ad9fa05ff10d3fed2703b524872d87e6bf
Lee Davies <lee.davies@condenast.com>
2020-04-14 12:33:07 +0100

    fixing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders pokemon lookbook", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appHeader = getByText(/Pokémon lookbook/i);
+  expect(appHeader).toBeInTheDocument();
 });
